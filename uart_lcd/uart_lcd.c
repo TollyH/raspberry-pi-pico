5b48c5287ff197fa744805b0385e62c4b8de1b5a
TollyH <ptolemyhill@sky.com>
2023-08-29 00:11:04 +0100

    GPIO initialisation

--- uart_lcd/uart_lcd.c
+++ uart_lcd/uart_lcd.c
@@ -17,6 +17,38 @@
 #define LCD_D7_PIN 11
 #define LCD_A_PIN 12
 
+#define PROMPT_STR "> "
+
+void lcd_init_gpio(void) {
+    gpio_init(LCD_RS_PIN);
+    gpio_init(LCD_E_PIN);
+    gpio_init(LCD_D0_PIN);
+    gpio_init(LCD_D1_PIN);
+    gpio_init(LCD_D2_PIN);
+    gpio_init(LCD_D3_PIN);
+    gpio_init(LCD_D4_PIN);
+    gpio_init(LCD_D5_PIN);
+    gpio_init(LCD_D6_PIN);
+    gpio_init(LCD_D7_PIN);
+    gpio_init(LCD_A_PIN);
+
+    gpio_set_dir(LCD_RS_PIN, GPIO_OUT);
+    gpio_set_dir(LCD_E_PIN, GPIO_OUT);
+    gpio_set_dir(LCD_D0_PIN, GPIO_OUT);
+    gpio_set_dir(LCD_D1_PIN, GPIO_OUT);
+    gpio_set_dir(LCD_D2_PIN, GPIO_OUT);
+    gpio_set_dir(LCD_D3_PIN, GPIO_OUT);
+    gpio_set_dir(LCD_D4_PIN, GPIO_OUT);
+    gpio_set_dir(LCD_D5_PIN, GPIO_OUT);
+    gpio_set_dir(LCD_D6_PIN, GPIO_OUT);
+    gpio_set_dir(LCD_D7_PIN, GPIO_OUT);
+    gpio_set_dir(LCD_A_PIN, GPIO_OUT);
+}
+
 int main() {
     stdio_init_all();
+
+    lcd_init_gpio();
+
+    printf("LCD <-> UART Controller. Commands start with #, i.e. \"#help\"\n");
 }
